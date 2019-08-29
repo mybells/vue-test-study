ec8b3aaf7442a2042a48580118106039a16be54d
mybells <435541908@qq.com>
2019-08-29 17:15:08 +0800

    Array.from()

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import mybells from 'mybellss';
 import {store} from './vuex/store.js';
 import 'element-ui/lib/theme-chalk/index.css';
 
-import './模块js/ES6的Module/moudle'
+// import './模块js/ES6的Module/moudle'
 // import './模块js/CommonJS模块化/CommonJS'
 // import './模块js/AMD模块化/amd'
 // import './模块js/CMD模块化/cmd'
